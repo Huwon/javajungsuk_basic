6b1ca39015f2907a31cf257d222c3cca0c35fa7d
DESKTOP-TI1CPI1\User <wlsgmldidhd@naver.com>
2020-12-21 18:36:22 +0900

    연산자의 결합규칙

---
+++ cho3/ex3_1.java
@@ -0,0 +1,16 @@
+package ch03;
+
+public class ex3_1 {
+
+	public static void main(String[] args) {
+		//연산자의 결합규칙
+		
+		int x, y;
+		
+		x=y=3; //y에 3이 저장된 후에, x에 3이 저장
+		System.out.println("x=" + x);
+		System.out.println("y="+y);
+
+	}
+
+}
